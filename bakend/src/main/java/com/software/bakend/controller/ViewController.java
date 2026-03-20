9434fed6977786d3fd4ad511ec1791231fc18485
karthic-rag <karthicragunath7@gmail.com>
2026-03-20 10:49:06 +0530

    fixed application files and added deployment necessity

---
+++ bakend/src/main/java/com/software/bakend/controller/ViewController.java
@@ -0,0 +1,13 @@
+package com.software.bakend.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/*/{y:[\\w\\-]+}" })
+    public String getIndex() {
+        return "forward:/index.html";
+    }
+}
